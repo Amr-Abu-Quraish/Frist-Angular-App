b4ccc60b648271cffb53c3a3c667f46dd4e59f0f
Amr-Abu-Quraish <amrhardi1@gmail.com>
2025-01-05 21:32:18 +0200

    edit slider

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { findIndex } from 'rxjs';
 
 interface Iitem {
   image: string
@@ -37,19 +38,34 @@ export class PortfolioComponent {
 
   ]
 
+
+
   isModalOpen = false;
-  selectedImage = '';
+  currentIndex: number = 0;
 
+  get selectedImage(): Iitem {
+    return this.itemList[this.currentIndex];
+  }
 
-  openModal(item: Iitem) {
-    this.selectedImage = item.image;
+  openModal(index: number): void {
+    console.log('Opening modal with index:', index);
+    this.currentIndex = index;
     this.isModalOpen = true;
   }
 
-
-  closeModal() {
+  closeModal(): void {
     this.isModalOpen = false;
   }
 
+  nextImage(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.itemList.length;
+    console.log('Next image index:', this.currentIndex);
+  }
+
+  prevImage(): void {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.itemList.length) % this.itemList.length;
+    console.log('Previous image index:', this.currentIndex);
+  }
 
 }
